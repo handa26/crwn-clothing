e0521c79268a8bd5c2b11937dc27e0a2f7c23829
Handa <anandamuhammadmtq@gmail.com>
2021-03-06 14:41:26 +0800

    Add custom form and button in auth page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignUpAndSignIn = () => (
+  <div className='sign-up-and-sign-in'>
+    <SignIn />
+  </div>
+);
+
+export default SignUpAndSignIn;
