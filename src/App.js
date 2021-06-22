4f42c6333512b18edb6dc3cc36f2e136edcf2420
handa26 <anandamuhammadmtq@gmail.com>
2021-06-23 00:35:53 +0800

    Add shop page and item collections component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const About = () => {
-  return <h1>About page</h1>;
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/about' component={About} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 }
