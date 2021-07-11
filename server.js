e83044e9ce7a7f735f583e50d847d1f122546524
handa26 <anandamuhammadmtq@gmail.com>
2021-07-11 23:36:28 +0800

    Fix naming

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/public")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/public", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
