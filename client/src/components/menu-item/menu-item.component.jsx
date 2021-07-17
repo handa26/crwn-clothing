c582fc72e509e2a6b290e82a1b4bd15cf280a019
handa26 <anandamuhammadmtq@gmail.com>
2021-07-17 21:09:43 +0800

    Implement mobile responsiveness

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,30 @@
 import { withRouter } from "react-router-dom";
 
-import "./menu-item.styles.scss";
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  Title,
+  Subtitle,
+} from "./menu-item.styles";
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
   return (
-    <div
-      className={`${size} menu-item`}
+    <MenuItemContainer
+      size={size}
       onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-      <div
+      <BackgroundImageContainer
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP</span>
-      </div>
-    </div>
+      <ContentContainer>
+        <Title className='title'>{title.toUpperCase()}</Title>
+        <Subtitle className='subtitle'>SHOP</Subtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   );
 };
 
